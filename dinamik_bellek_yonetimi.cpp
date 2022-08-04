0e4767cefe995ad1a04764f476d5474faf06cf91
Emre Karay <81099349+emrekaray@users.noreply.github.com>
2022-08-04 11:29:25 +0300

    Add files via upload
    malloc free fonksiyonları

---
+++ dinamik_bellek_yonetimi.cpp
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main() {
+	
+	//ORNEK 1 
+	
+/*	int a[40];
+	int *ptr;
+	
+	ptr = (int*)malloc(40*sizeof(int));
+	
+	ptr[0]= 10; 
+	ptr[1]=400;
+	
+	printf("%d %d\n",ptr[0],ptr[1]);
+	
+	free(ptr);
+	printf("%d %d\n",ptr[0],ptr[1]);*/
+	
+	// ORNEK 2 
+	
+/*	int boyut;
+	printf("dizi buyuklugu gir = ");
+	scanf("%d",&boyut);
+	int *ptr;
+	ptr = (int*)malloc(boyut*sizeof(int));
+	free(ptr);*/
+	
+	 
+	return 0;
+}
